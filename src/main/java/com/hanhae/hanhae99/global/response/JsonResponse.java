14c7824b895e0f48f66b16b4c970fdcbc80c3a39
Domae-back-end <ty_ty123@naver.com>
2023-06-23 17:06:12 +0900

    feat: success, error 나눠서 Response

---
+++ src/main/java/com/hanhae/hanhae99/global/response/JsonResponse.java
@@ -0,0 +1,22 @@
+package com.hanhae.hanhae99.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JsonResponse<T> {
+
+    private String result_code;
+    private T result;
+
+
+    public static <T> JsonResponse<T> error(String errorcode, T errorCode){
+        return new JsonResponse<>(errorcode,errorCode);
+    }
+
+    public static <T> JsonResponse<T> success(T result){
+        return new JsonResponse<>("Success",result);
+    }
+
+}
