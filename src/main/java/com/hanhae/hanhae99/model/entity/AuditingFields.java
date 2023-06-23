a424306a1cd5f9e125fe9e46d0a04184666014ef
Domae-back-end <ty_ty123@naver.com>
2023-06-23 17:05:32 +0900

    feat: Auditing추가

---
+++ src/main/java/com/hanhae/hanhae99/model/entity/AuditingFields.java
@@ -0,0 +1,31 @@
+package com.hanhae.hanhae99.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditingFields {
+
+    @CreatedDate
+    @Column( updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+
+}
