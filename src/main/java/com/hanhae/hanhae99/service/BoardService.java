20259bdbef24a4652c3a583f7cfa7d020a168af6
Domae-back-end <ty_ty123@naver.com>
2023-06-23 17:06:56 +0900

    feat: BoardService 기능 추가

---
+++ src/main/java/com/hanhae/hanhae99/service/BoardService.java
@@ -0,0 +1,84 @@
+package com.hanhae.hanhae99.service;
+
+import com.hanhae.hanhae99.global.exception.CustomException;
+import com.hanhae.hanhae99.global.type.ErrorCode;
+import com.hanhae.hanhae99.model.entity.Board;
+import com.hanhae.hanhae99.model.request.BoardSaveRequest;
+import com.hanhae.hanhae99.model.response.BoardResponse;
+import com.hanhae.hanhae99.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository repository;
+
+    @Transactional
+    public BoardResponse save(BoardSaveRequest req) {
+        Board board = repository.save(Board.builder()
+                .title(req.title())
+                .name(req.name())
+                .content(req.content())
+                .password(req.password())
+                .build());
+        return changeEntity(board);
+    }
+
+    @Transactional(readOnly = true)
+    public List<BoardResponse> findAll() {
+        return repository.findAll().stream()
+                .map(a -> changeEntity(a))
+                .sorted(Comparator.comparing(BoardResponse::createdAt).reversed())
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public BoardResponse findById(Long id) {
+        Board board = repository.findById(id).orElseThrow();
+        return changeEntity(board);
+    }
+
+    @Transactional
+    public BoardResponse updateBoard(Long id, BoardSaveRequest req){
+        Board board = repository.findById(id).orElseThrow();
+        if(!(board.getPassword().equals(req.password()))){
+            //TODO 에러
+            throw new CustomException(ErrorCode.NO_PASSWORD);
+        }
+        board.setName(req.name());
+        board.setTitle(req.title());
+        board.setContent(req.content());
+        return changeEntity(board);
+    }
+
+    public String deleteBoard(Long id, String password){
+        Board board = repository.findById(id).orElseThrow();
+        if(!(board.getPassword().equals(password))){
+            //TODO 에러
+            throw new CustomException(ErrorCode.NO_PASSWORD);
+        }
+        repository.deleteById(id);
+        return "성공적으로 삭제되었습니다.";
+    }
+
+
+    public BoardResponse changeEntity(Board board) {
+
+        return new BoardResponse(board.getTitle(),
+                board.getName(),
+                board.getContent(),
+                board.getCreatedAt().toString()
+        );
+    }
+
+}
