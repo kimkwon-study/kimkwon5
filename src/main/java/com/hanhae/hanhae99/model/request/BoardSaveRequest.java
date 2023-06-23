bf731f64e3cfb1c17be4be9d966ab2efcc1406bb
Domae-back-end <ty_ty123@naver.com>
2023-06-23 17:19:16 +0900

    update: 유효성 검사 추가

--- src/main/java/com/hanhae/hanhae99/model/request/BoardSaveRequest.java
+++ src/main/java/com/hanhae/hanhae99/model/request/BoardSaveRequest.java
@@ -1,15 +1,28 @@
 package com.hanhae.hanhae99.model.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record BoardSaveRequest(
         @NotNull
+        @NotBlank
+        @NotEmpty
         String title,
+
         @NotNull
+        @NotBlank
+        @NotEmpty
         String name,
+
         @NotNull
+        @NotBlank
+        @NotEmpty
         String password,
+
         @NotNull
+        @NotBlank
+        @NotEmpty
         String content
 ) {
 }
