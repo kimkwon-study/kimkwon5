d021b65c7a8bb5436a1958b4b49713ee86f322f9
Domae-back-end <ty_ty123@naver.com>
2023-06-23 15:13:59 +0900

    feat: spring 프로젝트 생성

---
+++ src/test/java/com/hanhae/hanhae99/Hanhae99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanhae.hanhae99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanhae99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
