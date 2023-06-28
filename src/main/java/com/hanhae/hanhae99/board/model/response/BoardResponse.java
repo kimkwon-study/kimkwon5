69019a49a2777c891aa3e295486024de71d905c1
Domae-back-end <ty_ty123@naver.com>
2023-06-28 18:46:10 +0900

    error: commont 쪽에서 에러

--- src/main/java/com/hanhae/hanhae99/board/model/response/BoardResponse.java
+++ src/main/java/com/hanhae/hanhae99/board/model/response/BoardResponse.java
@@ -1,9 +1,14 @@
 package com.hanhae.hanhae99.board.model.response;
 
+import com.hanhae.hanhae99.board.model.entity.Comment;
+
+import java.util.List;
+
 public record BoardResponse(
         String title,
         String name,
         String content,
-        String createdAt
+        String createdAt,
+        List<Comment> comments
 ) {
 }
