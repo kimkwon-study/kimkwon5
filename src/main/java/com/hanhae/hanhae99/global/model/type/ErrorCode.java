d69eb9efa160831637aeecb67f0252fd315b1482
Domae-back-end <ty_ty123@naver.com>
2023-06-28 17:47:45 +0900

    update: 에러코드 msg 수정

--- src/main/java/com/hanhae/hanhae99/global/model/type/ErrorCode.java
+++ src/main/java/com/hanhae/hanhae99/global/model/type/ErrorCode.java
@@ -8,20 +8,20 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     //Password
-    NO_PASSWORD(HttpStatus.UNAUTHORIZED, "사용자가 작성한 게시글이 아닙니다."),
+    NO_PASSWORD(HttpStatus.BAD_REQUEST, "작성자만 삭제/수정할 수 있습니다."),
     NO_PID(HttpStatus.BAD_REQUEST, "잘못된 게시글 정보입니다."),
 
     //Login / Register
-    LOGIN_NO(HttpStatus.UNAUTHORIZED, "아이디나 비밀번호가 맞지 않습니다."),
-    NAME_SAME(HttpStatus.UNAUTHORIZED, "중복되는 닉네임 입니다."),
+    LOGIN_NO(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다."),
+    NAME_SAME(HttpStatus.BAD_REQUEST, "중복된 username 입니다."),
 
     //Token
-    WRONG_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 토큰값 입니다."),
+    WRONG_TOKEN(HttpStatus.BAD_REQUEST, "토큰이 유효하지 않습니다."),
 
     //댓글
-    WRONG_BOARD_PID(HttpStatus.NOT_FOUND, "잘못된 게시글 접근 입니다."),
-    WRONG_COMMENT_PID(HttpStatus.NOT_FOUND, "잘못된 댓글 접근 입니다."),
-    WRONG_NAME(HttpStatus.NOT_FOUND, "사용자가 작성한 댓글이 아닙니다.")
+    WRONG_BOARD_PID(HttpStatus.BAD_REQUEST, "잘못된 게시글 접근 입니다."),
+    WRONG_COMMENT_PID(HttpStatus.BAD_REQUEST, "잘못된 댓글 접근 입니다."),
+    WRONG_NAME(HttpStatus.BAD_REQUEST, "작성자만 삭제/수정할 수 있습니다.")
     ;
 
     private final HttpStatus status;
