5880b3df175fc3d5c361dea81efac9caa93b53a3
Domae-back-end <ty_ty123@naver.com>
2023-06-28 17:47:08 +0900

    update: 댓글 내림차순을 위한 쿼리 매서드

--- src/main/java/com/hanhae/hanhae99/board/repository/CommentRepository.java
+++ src/main/java/com/hanhae/hanhae99/board/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.hanhae.hanhae99.board.repository;
 
+import com.hanhae.hanhae99.board.model.entity.Board;
 import com.hanhae.hanhae99.board.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<CommentRepository> findAllByOrderByCreatedAtDesc();
 }
