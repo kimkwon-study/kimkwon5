c8e763a2ab1b68d3b63910c4fdd97d45bbaeed95
Domae-back-end <ty_ty123@naver.com>
2023-06-28 17:47:17 +0900

    update: Auditing 추가

--- src/main/java/com/hanhae/hanhae99/board/model/entity/Comment.java
+++ src/main/java/com/hanhae/hanhae99/board/model/entity/Comment.java
@@ -1,6 +1,7 @@
 package com.hanhae.hanhae99.board.model.entity;
 
 import com.hanhae.hanhae99.board.model.request.CommentRequest;
+import com.hanhae.hanhae99.global.model.entity.AuditingFields;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -8,7 +9,7 @@ import lombok.*;
 @Getter
 @Builder
 @AllArgsConstructor
-public class Comment {
+public class Comment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
